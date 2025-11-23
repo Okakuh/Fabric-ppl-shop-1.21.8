fe059e1c7ad9c0b760b2b461164bcff4cc7472b3
Okakuh <migovycmark@gmail.com>
2025-11-23 11:47:29 +0100

    First

---
+++ src/main/java/net/okakuh/pplshop/PPLShopClient.java
@@ -0,0 +1,10 @@
+package net.okakuh.pplshop;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PPLShopClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
